84063ba2a2970002fbf3152b972852786ecefad0
HP <rahul.prabu.07@gmail.com>
2021-08-23 23:49:50 +0530

    Add project files.

---
+++ Day21_UserRegistration_ExceptionHandling/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day21_UserRegistration_ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Program with EXception Handling ");
+        }
+    }
+}
